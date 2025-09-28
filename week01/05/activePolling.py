0bc4cbb4079c17aba54a6cc7fb396870731a99ac
Wolos71 <105304486+Wolos71@users.noreply.github.com>
2025-09-28 19:24:29 +0200

    check every x second

--- week01/05/activePolling.py
+++ week01/05/activePolling.py
@@ -1,15 +1,21 @@
 import os
 import pathlib
+import time
 
 files = pathlib.Path("/Users/wolos/Documents/nauka/python_introduction/week01/04")
 cash = {}
 
+try:
+  while True:
+    for f in files.rglob("*"):
+      if f.is_file():
+        # print(os.stat(f))
+        times = os.stat(f)
+        cash [str(f.relative_to(files))] = times.st_mtime
+    print(cash)
+    time.sleep(5)
+except KeyboardInterrupt:
+  pass
 
-for f in files.rglob("*"):
-  if f.is_file():
-    # print(os.stat(f))
-    time = os.stat(f)
-    cash [str(f.relative_to(files))] = time.st_mtime
 
 
-print(cash)
