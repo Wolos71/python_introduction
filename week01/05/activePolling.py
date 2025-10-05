e10b8ad6b4efe8beae2fd083d648e2fd8bb2d067
Wolos71 <105304486+Wolos71@users.noreply.github.com>
2025-10-06 01:59:21 +0200

    activePolling od nowa i do końca

--- week01/05/activePolling.py
+++ week01/05/activePolling.py
@@ -2,20 +2,33 @@ import os
 import pathlib
 import time
 
-files = pathlib.Path("/Users/wolos/Documents/nauka/python_introduction/week01/04")
+files = pathlib.Path("")
 cash = {}
+
+
+                                                                
+for f in files.rglob("*"):
+  if f.is_file():
+    times = os.stat(f)
+    cash [str(f.relative_to(files))] = times.st_mtime  
+
+for path, times in cash.items():
+  print(f"{path} — {times}")
+
 
 try:
   while True:
+    time.sleep(8)
     for f in files.rglob("*"):
       if f.is_file():
-        # print(os.stat(f))
         times = os.stat(f)
-        cash [str(f.relative_to(files))] = times.st_mtime
-    print(cash)
-    time.sleep(5)
+        path = str(f.relative_to(files))
+        currentTime = times.st_mtime
+        if path not in cash:
+          print(f"nowy plik: {path} {currentTime}")
+          cash[path] = currentTime
+        elif cash[path] != currentTime:
+          cash[path] = currentTime
+          print(f"zmiana tu była: {path} {currentTime}")
 except KeyboardInterrupt:
-  pass
-
-
-
+  print("\n kończymy na dziś")
