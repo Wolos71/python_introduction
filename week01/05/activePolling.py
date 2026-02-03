c2fb2b186bf166b0bf60d1f1a642d1253bd6dc25
Wolos71 <105304486+Wolos71@users.noreply.github.com>
2026-02-03 19:26:47 +0100

    how much code week01 almost

--- week01/05/activePolling.py
+++ week01/05/activePolling.py
@@ -4,16 +4,16 @@ import time
 from datetime import datetime, timezone
 
 files = pathlib.Path("/users/Wolos/Documents/nauka/python_introduction")
-cash = {}
+cache = {}
 
 
                                                                 
 for f in files.rglob("*"):
-  if f.is_file():
+  if f.is_file() and not any(part.startswith(".") for part in f.parts):
     times = os.stat(f)
-    cash [str(f.relative_to(files))] = times.st_mtime  
+    cache [str(f.relative_to(files))] = times.st_mtime  
 
-for path, times in cash.items():
+for path, times in cache.items():
   print(f'{path}: {datetime.fromtimestamp(times).strftime("%Y-%m-%d %H:%M:%S")}')
 
 
@@ -21,15 +21,15 @@ try:
   while True:
     time.sleep(8)
     for f in files.rglob("*"):
-      if f.is_file():
+      if f.is_file() and not any(part.startswith(".") for part in f.parts):
         times = os.stat(f)
         path = str(f.relative_to(files))
         currentTime = times.st_mtime
-        if path not in cash:
+        if path not in cache:
           print(f'nowy plik: {path} {datetime.fromtimestamp(currentTime).strftime("%Y-%m-%d %H:%M:%S")}')
-          cash[path] = currentTime
-        elif cash[path] != currentTime:
-          cash[path] = currentTime
+          cache[path] = currentTime
+        elif cache[path] != currentTime:
+          cache[path] = currentTime
           print(f'zmiana tu była: {path} {datetime.fromtimestamp(currentTime).strftime("%Y-%m-%d %H:%M:%S")}')
 except KeyboardInterrupt:
   print("\n kończymy na dziś")
--- week01/howMuchCode.py
+++ week01/howMuchCode.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pathlib
+import time
 
 
 Langs = {
@@ -21,16 +22,19 @@ Langs = {
   ".sh": "Bash",
 }
 
-totalLang= {}
 
+dirToScan = sys.argv[1]
+dirpath = pathlib.Path(dirToScan)
+
+
+totalLang= {}
 for ext, lang in Langs.items():
     if lang not in totalLang:
         totalLang[lang] = 0 
 
 total = 0
 
-dirToScan = sys.argv[1]
-
+cache = {}
 
 
 def lineCount(fileArg):
@@ -42,46 +46,99 @@ def lineCount(fileArg):
   return lines
 
 
-
-for path, dirs, files in os.walk(dirToScan):
-  # print("sciezka:", path)
-  # print("pliki:", files)
-  path_elements = path.split(os.path.sep)                   #rozbija ściezke na katalogi
-  if any([x.startswith(".") for x in path_elements]):       #sprawdz czy katalog zaczyna się na . i pomiija
-    continue
-
-  for file in files:
-    if file.startswith("."):
-      continue
-
-    fullPath = f"{path}/{file}"
-    _, ext = os.path.splitext(file)                         #wycina rozszerzenie
-    ext = ext.lower()
-
+for f in dirpath.rglob("*"):
+  if f.is_file():
+    times = os.stat(f)
+    plik = str(f.relative_to(dirpath))
+    cache [plik] = times.st_mtime
+    ext = f.suffix.lower()      #.suffix wyciąga tylko rozszerzenie z obiektu path
     if ext not in Langs:
       continue
 
-    # print(f"{fullPath}")
-    linie = lineCount(fullPath)
-    total += linie
     lang = Langs[ext]
+    linie = lineCount(f)
     totalLang[lang] += linie
-    print(f"{fullPath}: {linie} {lang}")
+    total += linie
 
-
-for lang, lines in totalLang.items():
-  if lines != 0:
-    print(f"{lang}: {lines}")
-  else:
-    continue
-
-
-print(f"w sumie wszystko: {total}")
-
+    print(f"{plik}: {linie} linii ({lang})")
 
 
     
 
-  
+try:
+  while True:
+    time.sleep(30)
+    for f in dirpath.rglob("*"):
+      if f.is_file() and not any(part.startswith(".") for part in f.parts):
+        times = os.stat(f)
+        path = str(f.relative_to(dirpath))
+        currentTime = times.st_mtime
+        ext = f.suffix.lower()
+        if ext not in Langs:
+          continue
+        
+        if path not in cache:
+          cache[path] = currentTime
+          linie = lineCount(f)
+          totalLang[lang] += linie
+          total += linie
+
+          print(f":Nowy plik: {path} — {linie} linii ({lang})")
+
+        elif cache[path] != currentTime:
+          cache[path] = currentTime
+          linie = lineCount(f)
+          totalLang[lang] += linie
+          total += linie
 
 
+    print("\033[2J\033[H", end="")
+    for lang, lines in totalLang.items():
+      if lines != 0:
+        print(f"{lang}: {lines} linii")
+    print(f"Łącznie: {total} linii\n")
+    
+
+except KeyboardInterrupt:
+  print("\n kończymy na dziś")
+
+
+
+
+
+
+
+# for path, dirs, files in os.walk(dirToScan):
+#   # print("sciezka:", path)
+#   # print("pliki:", files)
+#   path_elements = path.split(os.path.sep)                   #rozbija ściezke na katalogi
+#   if any([x.startswith(".") for x in path_elements]):       #sprawdz czy katalog zaczyna się na . i pomiija
+#     continue
+
+#   for file in files:
+#     if file.startswith("."):
+#       continue
+
+#     fullPath = f"{path}/{file}"
+#     _, ext = os.path.splitext(file)                         #wycina rozszerzenie
+#     ext = ext.lower()
+
+#     if ext not in Langs:
+#       continue
+
+#     # print(f"{fullPath}")
+#     linie = lineCount(fullPath)
+#     total += linie
+#     lang = Langs[ext]
+#     totalLang[lang] += linie
+#     print(f"{fullPath}: {linie} {lang}")
+
+
+# for lang, lines in totalLang.items():
+#   if lines != 0:
+#     print(f"{lang}: {lines}")
+#   else:
+#     continue
+
+
+# print(f"w sumie wszystko: {total}")
