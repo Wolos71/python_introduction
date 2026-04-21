de0d4be4c7f9993ed244337f471efd836d7ef8a7
Wolos71 <105304486+Wolos71@users.noreply.github.com>
2026-04-21 16:11:21 +0200

    wyszukiwanie pogody po mieście

--- week02/free api exercise/weather.py
+++ week02/free api exercise/weather.py
@@ -1,10 +1,28 @@
 import requests
 
 
-res = requests.get('https://api.open-meteo.com/v1/forecast?latitude=52.23&longitude=21.01&current=temperature_2m,wind_speed_10m,weather_code')
+def prognoza(citys):
+    res = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={citys}&count=1&language=pl')
+    res_city = res.json()
+    try:
+        long = res_city['results'][0]['longitude']
+        lat = res_city['results'][0]['latitude']
+    except KeyError:
+        return None
 
-res_dict = res.json()
-temp = str(res_dict['current']['temperature_2m']) + res_dict['current_units']['temperature_2m']
-wind = str(res_dict['current']['wind_speed_10m']) + res_dict['current_units']['wind_speed_10m']
+    return lat, long
 
-print(f'temperatura: {temp}\nwiatr: {wind}')
+city = input('podaj miasto\n')
+
+if prognoza(city) is None:
+    print("nie ma takiej miejscowości")
+
+else:
+    lat, long = prognoza(city)
+    res = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current=temperature_2m,wind_speed_10m,weather_code')
+
+    res_dict = res.json()
+    temp = str(res_dict['current']['temperature_2m']) + res_dict['current_units']['temperature_2m']
+    wind = str(res_dict['current']['wind_speed_10m']) + res_dict['current_units']['wind_speed_10m']
+
+    print(f'temperatura: {temp}\nwiatr: {wind}')
