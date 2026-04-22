613e59f52369bbd3f61ceddfdc747c06030fd7ba
Wolos71 <105304486+Wolos71@users.noreply.github.com>
2026-04-22 15:35:41 +0200

    funkcja do prognozy na następne dni

--- week02/free api exercise/weather.py
+++ week02/free api exercise/weather.py
@@ -12,17 +12,39 @@ def prognoza(citys):
 
     return lat, long
 
-city = input('podaj miasto\n')
 
-if prognoza(city) is None:
+
+
+def dlugo_terminowa(lat, long, days):
+    res = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&daily=temperature_2m_mean&forecast_days={days}')
+    
+    res_dluga = res.json()
+    day = res_dluga['daily']['time']
+    temp = res_dluga['daily']['temperature_2m_mean']
+    print('długoterminowa:')
+    for i, j in zip(day, temp):
+        print(f'{i}: {j}')
+
+city = input('podaj miasto\n')
+days = input('na ile dni podać prognozy?\n')
+
+x = prognoza(city)
+
+
+
+if x is None:
     print("nie ma takiej miejscowości")
 
 else:
-    lat, long = prognoza(city)
+    lat, long = x
+    dlugo_terminowa(lat, long, days)
+
     res = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current=temperature_2m,wind_speed_10m,weather_code')
 
     res_dict = res.json()
     temp = str(res_dict['current']['temperature_2m']) + res_dict['current_units']['temperature_2m']
     wind = str(res_dict['current']['wind_speed_10m']) + res_dict['current_units']['wind_speed_10m']
 
-    print(f'temperatura: {temp}\nwiatr: {wind}')
+
+
+    print(f'\nDzisiejsza:\ntemperatura: {temp}\nwiatr: {wind}')
